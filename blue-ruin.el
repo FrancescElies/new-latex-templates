87e3255fb0dba876424e9e4e27a6678483385a0f
Francesc Elies Henar <francesc.elies@gmail.com>
2016-12-25 22:57:19 +0100

    sets gelasio

--- blue-ruin.el
+++ blue-ruin.el
@@ -16,7 +16,7 @@
   \\defaultfontfeatures{Mapping=tex-text,Scale=MatchLowercase}
   \\setromanfont{Garamond Premier Pro}
  \\setsansfont{ChunkFive}
-  \\setmonofont{Myriad Pro}
+  \\setmonofont{Gelasio}
 \\else
   \\usepackage[mathletters]{ucs}
   \\usepackage[utf8x]{inputenc}
@@ -52,8 +52,8 @@
 
 %This macro is to make cleaner the specification of the titling font
 \\newfontfamily\\mytitlefont[Color={FB782C}]{ChunkFive}
-\\newfontfamily\\myauthorfont[Color={FB782C}]{Gill Sans Display MT Pro}
-\\newfontfamily\\mybluefont[Color=electricblue]{Gill Sans Display MT Pro}
+\\newfontfamily\\myauthorfont[Color={FB782C}]{Gelasio}
+\\newfontfamily\\mybluefont[Color=electricblue]{Gelasio}
 \\DeclareTextFontCommand{\\textbf}{\\rmfamily\\color{electricblue}}
 \\DeclareTextFontCommand{\\textit}{\\\itshape\\color{electricblue}}
 
