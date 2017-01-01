aa25b6e7a8dfd8b8c9fe9983f38acc4560acc1e5
Francesc Elies Henar <francesc.elies@gmail.com>
2017-01-01 16:49:04 +0100

    fix

--- latex-templates/blue-ruin-no-cover.el
+++ latex-templates/blue-ruin-no-cover.el
@@ -2,7 +2,7 @@
 
 
 (add-to-list 'org-latex-classes
-             `((f-base buffer-file-name)
+             `(,(f-base buffer-file-name)
                ,(f-read
                  (replace-regexp-in-string "\.el" "\.tex" buffer-file-name))
                ("\\section{%s}" . "\\section*{%s}")
--- latex-templates/blue-ruin.el
+++ latex-templates/blue-ruin.el
@@ -1,7 +1,7 @@
 (provide 'blue-ruin)
 
 (add-to-list 'org-latex-classes
-             `((f-base buffer-file-name)
+             `(,(f-base buffer-file-name)
                ,(f-read
                  (replace-regexp-in-string "\.el" "\.tex" buffer-file-name))
                ("\\section{%s}" . "\\section*{%s}")
